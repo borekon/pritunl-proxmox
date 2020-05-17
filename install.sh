7871459960f50bbd90bb96e0de20ea7d87664b1d
Alfonso <vivancos@outlook.es>
2020-05-17 12:20:30 +0200

    wireguard added
    Added installation of wireguard and changed some syntax to the stretch version (i.e apt instead of apt-get)

--- install.sh
+++ install.sh
@@ -8,13 +8,18 @@ sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
 deb https://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main
 EOF
 
-apt-get install dirmngr
+echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
+
+apt install -y dirmngr
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
 apt-get update
-apt-get --assume-yes install pritunl mongodb-server
+apt install -y pritunl mongodb-server
 systemctl start mongodb pritunl
 systemctl enable mongodb pritunl
+echo 'Installing wireguard...'
+apt install -y wireguard-dkms wireguard-tools
 
 sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf'
 sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
