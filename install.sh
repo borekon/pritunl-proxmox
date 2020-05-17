6f77204dd255d4935967c0ebc87acd906ca79864
Alfonso <vivancos@outlook.es>
2020-05-17 12:17:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@ tee -a /etc/apt/sources.list.d/pritunl.list << EOF
 deb http://repo.pritunl.com/stable/apt stretch main
 EOF
 
+sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
+deb https://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main
+EOF
+
 apt-get install dirmngr
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
 apt-get update
 apt-get --assume-yes install pritunl mongodb-server
 systemctl start mongodb pritunl
@@ -22,4 +27,9 @@ cd net
 mknod tun c 10 200
 chmod 666 tun
 
+echo 'Setup key:'
+echo ''
 pritunl setup-key
+echo 'First-time key:'
+echo ''
+pritunl default-password
